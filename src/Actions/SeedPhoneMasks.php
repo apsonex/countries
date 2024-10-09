329f8d3736340940de437dd8e468305de41587bf
Gurinder Chauhan <gurinderchauhan@gmail.com>
2024-10-09 19:52:33 -0400

    update

---
+++ src/Actions/SeedPhoneMasks.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Apsonex\Countries\Actions;
+
+use Apsonex\Countries\Models\Country;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class SeedPhoneMasks
+{
+
+    public static function seed()
+    {
+        collect(File::json(__DIR__ . '../../database/data/phone-masks.json'))
+            ->each(function ($mask, $iso2) {
+                Country::query()->where('', $iso2)->update([
+                    'phone_mask' => $mask
+                ]);
+            });
+    }
+}
